a2f2c18931f6e7131dba10f2036e3e82aa57658b
francescolucco <francescolucco@googlemail.com>
2021-10-20 15:46:36 +0200

    first-commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Chiedo all'utente di inserire nome, cognome, colore preferito.
+// Restituisco in output la password generata
+
+/* 
+1- inizializzare costanti nome, cognome, colore preferito
+2- operazione di concatenazione per creare password
+3- stampare dati
+*/ 
