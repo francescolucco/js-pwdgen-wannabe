ab32ef587eaccc070965ba5c19aa41877d4ce01d
francescolucco <francescolucco@googlemail.com>
2021-10-20 16:43:38 +0200

    add animation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@
 2- operazione di concatenazione per creare password
 3- stampare dati
 */ 
+
+const affermazione = prompt('Ora ti aiuteremo a creare una password indecifrabile. Sei pronto? (SI/NO)');
+const firstname = prompt('Qual è il tuo nome?');
+const surname = prompt('Qual è il tuo cognome?');
+const favoriteColor = prompt('Qual è il tuo colore preferito?');
+const year = 21
+
+console.log(firstname)
+console.log(surname)
+console.log(favoriteColor)
+
+const passwordGenerated = firstname + surname + favoriteColor + year
+
+console.log(passwordGenerated);
+
+document.getElementById('fl_passwordGenerata').innerHTML = 
+`
+La tua password generata è:
+<br>
+${passwordGenerated}
+`
