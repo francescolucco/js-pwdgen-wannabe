3bb0ae539c9d5d7b0219179deba697a5cce68d6d
francescolucco <francescolucco@googlemail.com>
2021-10-20 17:17:50 +0200

    add text-transform-uppercase

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ const affermazione = prompt('Ora ti aiuteremo a creare una password indecifrabil
 const firstname = prompt('Qual è il tuo nome?');
 const surname = prompt('Qual è il tuo cognome?');
 const favoriteColor = prompt('Qual è il tuo colore preferito?');
+const amountBitcoin = parseInt(prompt('Quanti bitcoin possiedi?'));
 const year = 21
+const bitcoinEuro = 57317
 
 console.log(firstname)
 console.log(surname)
@@ -19,11 +21,24 @@ console.log(favoriteColor)
 
 const passwordGenerated = firstname + surname + favoriteColor + year
 
+const bitcoinValue = amountBitcoin * 57317
+
 console.log(passwordGenerated);
 
 document.getElementById('fl_passwordGenerata').innerHTML = 
 `
 La tua password generata è:
 <br>
+<h2>
 ${passwordGenerated}
+</h2>
+`
+
+document.getElementById('fl_value-bitcoin').innerHTML = 
+`
+Il valore in euro dei tuoi bitcoin è di:
+<br>
+<h1>
+${bitcoinValue} &euro;
+</h1>
 `
